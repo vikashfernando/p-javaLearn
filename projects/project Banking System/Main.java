774a23356c2876ff6e786ce29550a2186b70b53f
vikashfernando <vikashdeshitha@gmail.com>
2026-03-24 01:37:24 +0530

    main.java created

--- projects/project Banking System/Main.java
+++ projects/project Banking System/Main.java
@@ -1,2 +1,21 @@
-public class Main {
+public class Main{
+    public static void main(String[] args) {
+
+        BankAccount account1 = new SavingsAccount("Vikash", 1001, 5000);
+        BankAccount account2 = new CurrentAccount("Nimal", 1002, 3000, 2000);
+
+        System.out.println("---- Savings Account ----");
+        account1.displayDetails();
+        account1.deposit(1000);
+        account1.withdraw(2000);
+        System.out.println("Updated Balance: " + account1.getBalance());
+
+        System.out.println();
+
+        System.out.println("---- Current Account ----");
+        account2.displayDetails();
+        account2.deposit(500);
+        account2.withdraw(4000);
+        System.out.println("Updated Balance: " + account2.getBalance());
+    }
 }
