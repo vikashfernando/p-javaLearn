14ddeae76b1bae9f37a0304dfd4c148faa7a95c9
vikashfernando <vikashdeshitha@gmail.com>
2026-03-24 01:35:44 +0530

    bank account.java created

--- projects/project Banking System/BankAccount.java
+++ projects/project Banking System/BankAccount.java
@@ -1,2 +1,44 @@
-public class BankAccount {
+abstract class BankAccount {
+    private String accountHolderName;
+    private int accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolderName, int accountNumber, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    protected void deductBalance(double amount) {
+        balance -= amount;
+    }
+
+    public abstract void withdraw(double amount);
+
+    public void displayDetails() {
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+    }
 }
