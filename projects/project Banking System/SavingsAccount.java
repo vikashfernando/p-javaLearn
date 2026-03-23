295f6e6bade4403ea0a7d8229c94bfd3639e9e8d
vikashfernando <vikashdeshitha@gmail.com>
2026-03-24 01:36:19 +0530

    saving account.java created

--- projects/project Banking System/SavingsAccount.java
+++ projects/project Banking System/SavingsAccount.java
@@ -1,2 +1,16 @@
-public class SavingsAccount {
+class SavingsAccount extends BankAccount {
+
+    public SavingsAccount(String accountHolderName, int accountNumber, double balance) {
+        super(accountHolderName, accountNumber, balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= getBalance()) {
+            deductBalance(amount);
+            System.out.println("Withdrawn from Savings Account: " + amount);
+        } else {
+            System.out.println("Invalid amount or insufficient balance in Savings Account");
+        }
+    }
 }
