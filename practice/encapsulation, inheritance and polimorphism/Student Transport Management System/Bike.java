62d831514a1f44136990fb59e13a2de0f4fb3d88
vikashfernando <vikashdeshitha@gmail.com>
2026-04-08 00:35:50 +0530

    bike class done + added inheritajnce

--- practice/encapsulation, inheritance and polimorphism/Student Transport Management System/Bike.java
+++ practice/encapsulation, inheritance and polimorphism/Student Transport Management System/Bike.java
@@ -1,5 +1,29 @@
-public class Bike{
+public class Bike extends Transport{
     private int parkingFee;
     private String helmetNumber;
 
+    public Bike(String vehicleId,String route,int distance,int parkingFee,String helmetNumber){
+        super(vehicleId,route,distance);
+        this.helmetNumber=helmetNumber;
+        setParkingFee(parkingFee);
+    }
+
+    public int getParkingFee(){
+        return this.parkingFee;
+    }
+    public String getHelmetNumber(){
+        return this.helmetNumber;
+    }
+
+    public void setHelmetNumber(String helmetNumber){
+        this.helmetNumber=helmetNumber;
+    }
+    public void setParkingFee(int parkingFee){
+        if(parkingFee>0){
+            this.parkingFee=parkingFee;
+        }else{
+            System.out.println("invalid parking fee amount...");
+        }
+    }
 }
+
