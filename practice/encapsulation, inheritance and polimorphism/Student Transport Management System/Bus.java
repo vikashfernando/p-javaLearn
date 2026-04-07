7f4d921e0b647d87d34a3af2a4c529d3cf4a69c3
vikashfernando <vikashdeshitha@gmail.com>
2026-04-08 00:16:24 +0530

    applied inheritnce for bus

--- practice/encapsulation, inheritance and polimorphism/Student Transport Management System/Bus.java
+++ practice/encapsulation, inheritance and polimorphism/Student Transport Management System/Bus.java
@@ -1,5 +1,32 @@
-public class Bus{
+public class Bus extends Transport{
     private int capacity;
     private int feePerKm;
-    
+
+    public Bus(String vehicleId,String route,int distance,int capacity,int feePerKm){
+        super(vehicleId,route,distance);
+        this.capacity=capacity;
+        this.feePerKm=feePerKm;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+    public int getFeePerKm(){
+        return this.feePerKm;
+    }
+    public void setCapacity(int capacity){
+        if(capacity>0){
+            this.capacity=capacity;
+        }else{
+            System.out.println("invalid number, pls check again");
+        }
+    }
+    public void setFeePerKm(int feePerKm){
+        if(feePerKm>0){
+            this.feePerKm=feePerKm;
+        }else{
+            System.out.println("invalid amount...");
+        }
+    }
+
 }
